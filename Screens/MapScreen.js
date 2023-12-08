16628fe03bee5a3f1f1915b4a2affe8fef9f218b
odhis101 <joshodhiambo5@gmail.com>
2023-12-08 22:36:37 +0300

    updates

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -24,9 +24,8 @@ const MapScreen = ({navigation}) => {
 
   return (
     <View>
-
-      <TouchableOpacity style={tw`absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg bg-gray-200 `}
       
+      <TouchableOpacity style={tw`absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg bg-gray-200 `}
       onPress={logout}>
         <Icon
         name="home"/>
