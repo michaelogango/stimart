67ae168ff356b5de677bd688c51ece8c9601502b
odhis101 <joshodhiambo5@gmail.com>
2023-12-08 22:43:45 +0300

    updates

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -9,6 +9,7 @@ import NavigationCard from '../Component/NavigationCard';
 import RideOptions from '../Component/RideOptions';
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import asun
 
 
 const MapScreen = ({navigation}) => {
